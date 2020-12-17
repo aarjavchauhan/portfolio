368f82144fea57079270166763f92a2bfe6c0379
Aarjav Chauhan <aarjavchauhan439@gmail.com>
2020-12-17 15:01:27 -0700

    added CV and link to pdf

--- aarjav/src/components/HelloWorld.vue
+++ aarjav/src/components/HelloWorld.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <hr />
+    <h4>aarjav.chauhan@colorado.edu</h4>
+    <a rel="icon" href="/aarjavCV.pdf" target="_blank">curriculum vitae</a>
+    <h2>other projects &#8595;</h2>
+    <h5>haiku generator</h5>
+    <hr />
   </div>
 </template>
 
@@ -11,21 +17,11 @@ export default {
     msg: String
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
 a {
   color: #42b983;
 }
