38be5edeaa3164f4332976a515d0ca4c1828ff88
Aarjav Chauhan <aarjavchauhan439@gmail.com>
2020-12-18 15:20:11 -0700

    added router-view transitions and custom argesta font file

--- aarjav/src/App.vue
+++ aarjav/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="App">
-    <router-view />
+    <router-view v-slot="{ Component}">
+      <transition name="fade">
+        <component :is="Component" />
+      </transition>
+    </router-view>
   </div>
 </template>
 
@@ -13,12 +17,35 @@ export default {
 </script>
 
 <style>
+
+@font-face {
+  font-family: "Argesta";
+  src: local("Argesta"),   url(./fonts/argestadisplay-regular-webfont.ttf) format("truetype");
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  /* letter-spacing: 2vw; */
 }
+
+.fade-enter-active
+ {
+  transition: opacity 1s ease;
+}
+
+.fade-leave-active
+{
+  transition: opacity 0s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
 </style>
